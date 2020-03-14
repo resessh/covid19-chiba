a289bfcdd18838239de89d429bdfa68ef815ea49
resessh <info@resessh.net>
2020-03-15 01:07:41 +0900

    temp

---
+++ tool/news-updater/data.ts
@@ -0,0 +1,46 @@
+import path from 'path'
+import { promises as fs } from 'fs'
+import { compareDesc, parseISO } from 'date-fns'
+import beautify from 'json-beautify'
+import { NewsItem } from './types'
+
+type NewsData = {
+  newsItems: NewsItem[]
+}
+
+const newsDataPath = path.join(__dirname, '../../data/news.json')
+
+const readData = async (): Promise<NewsData> => {
+  const data = (await fs.readFile(newsDataPath)).toString()
+  try {
+    const parsedData: NewsData = JSON.parse(data)
+    return parsedData
+  } catch (error) {
+    throw new Error('Fail to parse news data.')
+  }
+}
+
+export const getDateOfLatestItem = async (): Promise<Date> => {
+  const existsData = (await readData()).newsItems
+  const sortedExistsData = existsData.sort((a, b) =>
+    compareDesc(parseISO(a.date), parseISO(b.date))
+  )
+  return sortedExistsData.length === 0
+    ? new Date(0)
+    : parseISO(sortedExistsData[0].date)
+}
+
+export const saveNewData = async (newItems: NewsItem[]) => {
+  const existsData: NewsData = await readData()
+  const newData: NewsData = {
+    ...existsData,
+    newsItems: [...existsData.newsItems, ...newItems].sort((a, b) =>
+      compareDesc(parseISO(a.date), parseISO(b.date))
+    )
+  }
+
+  await fs.writeFile(
+    newsDataPath,
+    beautify(newData, (null as any) as any[], 2, 100) // 本家の型定義が間違っているので無理やり通す
+  )
+}
